399b5be23709fd1ce8a87aabf2b8d06cb5e8a80c
Kelly Anderson <kean9528@colorado.edu>
2022-04-14 09:27:44 -0600

    delete endpoint

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -6,8 +6,25 @@ class Api::V1::SubscriptionsController < ApplicationController
   end
 
   def create
+    # require "pry"; binding.pry
+    customer = User.find(params[:subscription][:user_id])
+    subscription = customer.subscriptions.create(title: params[:subscription][:title], price: params[:subscription][:price], frequency: params[:subscription][:frequency], teas: params[:subscription][:teas])
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription), status: 201
+    end
   end
 
-  def delete
+  def destroy
+    subscription = Subscription.find(params[:id])
+    tea_subscription = TeaSubscription.find_by(subscription_id: subscription.id)
+    tea_subscription.destroy
+    if subscription.destroy
+      render status: :no_content
+    end
   end
+
+  # private
+  #   def subscription_params
+  #     params.require(:subscription).permit(:title, :price, :status, :frequency, :teas, :user_id)
+  #   end
 end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -21,4 +21,38 @@ describe "Subscriptions API" do
     expect(subscriptions[:data][0][:attributes][:teas]).to be_a(Array)
     expect(subscriptions[:data][0][:attributes][:teas][0][:title]).to eq("Green Tea")
   end
+
+  it 'creates a new subscription for a user' do
+    user = User.create!(first_name: "Kelly", last_name: "Anderson", address: "1234 Laurel St", email: "kelly@gmail.com")
+    tea1 = Tea.create!(title: "Green Tea", description: "Green Tea", temperature: "100", brew_time: "4 minutes")
+    tea2 = Tea.create!(title: "Black Tea", description: "Black Tea", temperature: "150", brew_time: "7 minutes")
+    tea3 = Tea.create!(title: "White Tea", description: "White Tea", temperature: "200", brew_time: "2 minutes")
+    subscription_params = ({
+                title: "my first subscription",
+                price: "55",
+                status: "Active",
+                frequency: "Weekly",
+                teas: "#{[tea1,tea2,tea3]}",
+                user_id: "#{user.id}"
+              })
+    headers = {"CONTENT_TYPE" => "application/json"}
+    post "/api/v1/subscriptions", headers: headers, params: JSON.generate(subscription: subscription_params)
+    subscriptions = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+  end
+
+  it "can destroy an item" do
+    user = User.create!(first_name: "Kelly", last_name: "Anderson", address: "1234 Laurel St", email: "kelly@gmail.com")
+    tea1 = Tea.create!(title: "Green Tea", description: "Green Tea", temperature: "100", brew_time: "4 minutes")
+    tea2 = Tea.create!(title: "Black Tea", description: "Black Tea", temperature: "150", brew_time: "7 minutes")
+    subscription1 = user.subscriptions.create!(title: "my first subscription", price: "55", status: "Active", frequency: "Weekly")
+    tea_subscription = TeaSubscription.create!(subscription_id: subscription1.id, tea_id: tea1.id)
+    expect(Subscription.count).to eq(1)
+
+    delete "/api/v1/subscriptions/#{subscription1.id}"
+
+    expect(response).to be_successful
+    expect(Subscription.count).to eq(0)
+    expect{Subscription.find(subscription1.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
